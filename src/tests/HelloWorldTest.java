3b5b123cb1b3d307ee05232770cc8418245d5a57
Hulya Kaya <ysfky24@gmail.com>
2021-10-03 22:15:16 -0400

    updating  hello world test

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -3,5 +3,6 @@ package tests;
 public class HelloWorldTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("One more hello world");
     }
 }
