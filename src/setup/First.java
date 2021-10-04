a3cb67cc9b72995e980b7792f6f659d7116dcb34
Hulya Kaya <ysfky24@gmail.com>
2021-10-03 21:04:23 -0400

    updating first file

--- src/setup/First.java
+++ src/setup/First.java
@@ -2,7 +2,7 @@ package setup;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello universe");
 
         //this is a new line
         // i want to create a history
